84557f17c84afcc7392e140b12af98f8d29fcc52
Charleswcl <wang90cl@hotmail.com>
2023-01-20 16:26:32 +0800

    SPS Game

--- src/main/java/iss/App.java
+++ src/main/java/iss/App.java
@@ -1,17 +1,41 @@
 package iss;
 
-/**
- * Hello world!
- */
+import java.util.Scanner;
+
 public final class App {
     private App() {
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        String[] choice = { "scissor", "paper", "stone" };
+        Integer computerChoice, playerChoice;
+        String winner = "";
+
+        Scanner scanner = new Scanner(System.in);
+        SCP scp = new SCP();
+        computerChoice = scp.generateComputerChoice();
+
+        while (winner.equals("")) {
+            System.out.println("Enter (1)Scissor, (2)Paper, or (3)Stone: ");
+            playerChoice = scanner.nextInt();
+
+            if (playerChoice >= 1 && (playerChoice <= 3)) {
+
+            } else {
+                System.out.println("Invalid input: Only number 1, 2 or 3 is allowed.");
+                continue;
+            }
+
+            winner = scp.checkWinner(playerChoice, computerChoice);
+            if (winner.equalsIgnoreCase("")) {
+                System.out.println("It is a tie.");
+            } else {
+                System.out.println("winner is: " + winner);
+            }
+        }
+
+        scanner.close();
     }
+
 }
